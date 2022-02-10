8af78f53fd809698f4faea5375fae2799bfc5cbc
louprusak <loup.rusak@gmail.com>
2022-02-10 11:01:29 +0100

    commentaires string utils

--- string_utils.c
+++ string_utils.c
@@ -1,9 +1,11 @@
 #include "diary.h"
 
+//enlever le \n de la fin de la chaine de caractère
 void removeBackSlashN(char *s){
     s[strlen(s) -1] = '\0';
 }
 
+//récupérer une partie de la chaine de carcatères depuis un début et jusqu'à une fin
 void recupString(char* e, char*s, int debut,int fin){
     int j = 0;
     for (int i = debut; i < fin; i++)
